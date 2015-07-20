2e2cfec5f1b29e7000aea1814adac0b567d1037b
maxim <orlov.maxxim@gmail.com>
2015-07-20 18:43:26 +0300

    betta1 commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
-	console.log("File mndfgd js load good")
+
+/*	var n = prompt ('n?', '');
+
+	function sumTo (n) {
+		var res = 0;
+		for (var i=1; i<=n; i++) {
+			res = res + i;
+		}
+		return res;
+	}
+
+	var fff = function sumTo (n) {
+		if (n == 1) return 1;
+		return n + sumTo(n-1);
+	}
+
+	alert(fff(n));*/
 });

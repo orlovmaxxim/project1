7610dedca5e46360e9542dc1486045f9af4a31d0
maxim <orlov.maxxim@gmail.com>
2015-07-26 17:07:21 +0300

    add third page contact.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,3 @@
 $(function(){
 
-/*	var n = prompt ('n?', '');
-
-	function sumTo (n) {
-		var res = 0;
-		for (var i=1; i<=n; i++) {
-			res = res + i;
-		}
-		return res;
-	}
-
-	var fff = function sumTo (n) {
-		if (n == 1) return 1;
-		return n + sumTo(n-1);
-	}
-
-	alert(fff(n));*/
 });
